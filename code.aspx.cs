23260cb834f9ed85f411f754dc97e38e7f2bf6e2
TerryJ <terryj2008@163.com>
2018-04-20 10:22:51 +0800

    second commit

---
+++ code.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class code : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(Request.QueryString["openid"]))
+        {
+            Response.Redirect("http://mairuikeji.com/snsapi.aspx?state=" + HttpUtility.UrlEncode(Request.Url.AbsoluteUri));
+        }
+        else
+        {
+            Response.Write(Request.Url.AbsoluteUri);
+        }
+    }
+}
